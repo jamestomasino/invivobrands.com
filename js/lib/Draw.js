29b9aad7459f3d043ee76b99849f6de695b618ca
James Tomasino <james@tomasino.org>
2015-07-06 14:21:18 -0400

    added Draw class

---
+++ js/lib/Draw.js
@@ -0,0 +1,50 @@
+(function (NS) {
+
+
+	var Draw = {};
+
+	// Create lines to be added to the DOM.
+	// All lines get classname of "line"
+	// Style appropirately, for instance:
+	//		position: absolute;
+	//		height: 0px;
+	//		border-width: 1px 0px 0px 0px;
+	//		border-style: solid;
+	//		border-color: #666;
+	//		transform: translateZ(1px);
+	//		-webkit-backface-visibility: hidden;
+	//		-moz-backface-visibility: hidden;
+	//		-ms-backface-visibility: hidden;
+	//		-o-backface-visibility: hidden;
+	//		backface-visibility: hidden;
+
+
+	Draw.line = function (x1, y1, x2, y2)	{
+		var isIE = navigator.userAgent.indexOf("MSIE") > -1;
+		if (x2 < x1) {
+			var temp = x1; x1 = x2; x2 = temp;
+			temp = y1; y1 = y2; y2 = temp;
+		}
+		var line = document.createElement("div");
+		line.className = "line";
+		var length = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+		line.style.width = length + "px";
+		if (isIE) {
+			line.style.top = (y2 > y1) ? y1 + "px" : y2 + "px";
+			line.style.left = x1 + "px";
+			var nCos = (x2-x1)/length;
+			var nSin = (y2-y1)/length;
+			line.style.filter = "progid:DXImageTransform.Microsoft.Matrix(sizingMethod='auto expand', M11=" + nCos + ", M12=" + -1*nSin + ", M21=" + nSin + ", M22=" + nCos + ")";
+		} else {
+			var angle = Math.atan((y2-y1)/(x2-x1));
+			line.style.top = y1 + 0.5*length*Math.sin(angle) + "px";
+			line.style.left = x1 - 0.5*length*(1 - Math.cos(angle)) + "px";
+			line.style.MozTransform = line.style.WebkitTransform = line.style.OTransform= "rotate(" + angle + "rad)";
+		}
+		return line;
+	};
+
+	var namespace = new NS ( 'lib' );
+	namespace.Draw = Draw;
+
+})(window.NS);
