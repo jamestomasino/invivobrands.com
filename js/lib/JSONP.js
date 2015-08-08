604cd79742eaae882a91205267be1e69806c21b8
James Tomasino <james@tomasino.org>
2015-08-08 00:08:04 -0400

    more class syntax fixes

--- js/lib/JSONP.js
+++ js/lib/JSONP.js
@@ -1,32 +1,32 @@
 (function (NS) {
 	"use strict"
 
-	/*
-	jsonp('http://www.helloword.com/something.json', callback, error);
-	*/
+	function classWrapper() {
 
-	function JSONP(url, callbackFunction, errorFunction)
-	{
-		// Create temporary global callback function, but delete reference on use
-		var callbackName = 'JSONP_CALLBACK_' + Math.round(100000 * Math.random());
-		var script = document.createElement('script');
-		script.onerror = function(err) {
-			delete window[callbackName];
-			document.body.removeChild(script);
-			errorFunction(err);
-		};
+		return function (url, callbackFunction, errorFunction)
+		{
+			// Create temporary global callback function, but delete reference on use
+			var callbackName = 'JSONP_CALLBACK_' + Math.round(100000 * Math.random());
+			var script = document.createElement('script');
+			script.onerror = function(err) {
+				delete window[callbackName];
+				document.body.removeChild(script);
+				errorFunction(err);
+			};
 
-		window[callbackName] = function(data) {
-			delete window[callbackName];
-			document.body.removeChild(script);
-			callbackFunction(data);
-		};
+			window[callbackName] = function(data) {
+				delete window[callbackName];
+				document.body.removeChild(script);
+				callbackFunction(data);
+			};
 
-		script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
-		document.body.appendChild(script);
+			script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+			document.body.appendChild(script);
+		}
 	}
 
-	var namespace = new NS ( 'lib' );
-	namespace.JSONP = JSONP;
+	var libs = [];
+	var polyfills = [];
+	NS.load ( 'lib.JSONP', libs.concat(polyfills), classWrapper, this );
 
 })(window.NS);
--- js/lib/Prefix.js
+++ js/lib/Prefix.js
@@ -1,32 +1,36 @@
 (function (NS) {
 	"use strict";
 
-	var Prefix = function () {};
+	function classWrapper() {
+		var Prefix = function () {};
 
-	Prefix.hasStyle = function ( prefixlist ) {
-		var prefixes = prefixlist.split(' ');
-		var div = document.createElement('div');
-		for(var i = 0; i < prefixes.length; i++) {
-			if(div && div.style[prefixes[i]] !== undefined) {
-				return true;
+		Prefix.hasStyle = function ( prefixlist ) {
+			var prefixes = prefixlist.split(' ');
+			var div = document.createElement('div');
+			for(var i = 0; i < prefixes.length; i++) {
+				if(div && div.style[prefixes[i]] !== undefined) {
+					return true;
+				}
 			}
-		}
-		return false;
-	}
-
-	Prefix.getStyle = function ( prefixlist, fallback ) {
-		var prefixes = prefixlist.split(' ');
-		var div = document.createElement('div');
-		for(var i = 0; i < prefixes.length; i++) {
-			if(div && div.style[prefixes[i]] !== undefined) {
-				return prefixes[i];
-			}
+			return false;
 		}
 
-		return fallback;
+		Prefix.getStyle = function ( prefixlist, fallback ) {
+			var prefixes = prefixlist.split(' ');
+			var div = document.createElement('div');
+			for(var i = 0; i < prefixes.length; i++) {
+				if(div && div.style[prefixes[i]] !== undefined) {
+					return prefixes[i];
+				}
+			}
+
+			return fallback;
+		}
+
+		return Prefix;
 	}
 
-	var namespace = new NS ( 'lib' );
-	namespace.Prefix = Prefix;
-
+	var libs = [];
+	var polyfills = [];
+	NS.load ( 'lib.Prefix', libs.concat(polyfills), classWrapper, this );
 })(window.NS);
