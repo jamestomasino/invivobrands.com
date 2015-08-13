f9a3c14bfc5ebdca196d6be682ffe844a223144e
James Tomasino <james@tomasino.org>
2015-08-13 03:20:54 -0400

    better image compression for adaptive images

--- adaptive-images.php
+++ adaptive-images.php
@@ -12,9 +12,9 @@
 
 /* CONFIG ----------------------------------------------------------------------------------------------------------- */
 
-$resolutions   = array(1382, 992, 735, 620, 480); // the resolution break-points to use (screen widths, in pixels)
+$resolutions   = array(1600, 1382, 992, 735, 620, 480); // the resolution break-points to use (screen widths, in pixels)
 $cache_path    = "images-cache"; // where to store the generated re-sized images. Specify from your document root!
-$jpg_quality   = 80; // the quality of any generated JPGs on a scale of 0 to 100
+$jpg_quality   = 90; // the quality of any generated JPGs on a scale of 0 to 100
 $sharpen       = TRUE; // Shrinking images can blur details, perform a sharpen on re-scaled images?
 $watch_cache   = TRUE; // check that the adapted image isn't stale (ensures updated source images are re-cached)
 $browser_cache = 60*60*24*7; // How long the BROWSER cache should last (seconds, minutes, hours, days. 7days by default)
