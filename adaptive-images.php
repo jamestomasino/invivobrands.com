0b8247a55e1ce855d1cacc62996aa5c4581aa86b
James Tomasino <james@tomasino.org>
2015-08-13 02:39:10 -0400

    added breakpoints for adaptive images

--- adaptive-images.php
+++ adaptive-images.php
@@ -12,7 +12,7 @@
 
 /* CONFIG ----------------------------------------------------------------------------------------------------------- */
 
-$resolutions   = array(1382, 992, 768, 480); // the resolution break-points to use (screen widths, in pixels)
+$resolutions   = array(1382, 992, 735, 620, 480); // the resolution break-points to use (screen widths, in pixels)
 $cache_path    = "images-cache"; // where to store the generated re-sized images. Specify from your document root!
 $jpg_quality   = 80; // the quality of any generated JPGs on a scale of 0 to 100
 $sharpen       = TRUE; // Shrinking images can blur details, perform a sharpen on re-scaled images?
